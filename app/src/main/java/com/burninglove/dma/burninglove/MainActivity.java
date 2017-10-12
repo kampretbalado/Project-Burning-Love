3bb37650000c99f5316428e1dc2fe909ef0c5790
ashaliha <amalia.shaliha@gmail.com>
2017-10-12 18:48:24 +0700

    + tampilan menu utama

--- app/src/main/java/com/burninglove/dma/burninglove/MainActivity.java
+++ app/src/main/java/com/burninglove/dma/burninglove/MainActivity.java
@@ -1,13 +1,28 @@
 package com.burninglove.dma.burninglove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button storyModeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        storyModeButton = (Button) findViewById(R.id.story_mode_button);
+
+        storyModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
