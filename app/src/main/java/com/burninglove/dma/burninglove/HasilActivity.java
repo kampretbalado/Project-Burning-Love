a890235d27b8320aead70d1694571ba459ed4409
DwikiSM <dwikism@gmail.com>
2017-11-09 00:55:50 +0700

    +sinkron sederhana data total kalori yang dibakar

--- app/src/main/java/com/burninglove/dma/burninglove/HasilActivity.java
+++ app/src/main/java/com/burninglove/dma/burninglove/HasilActivity.java
@@ -1,6 +1,9 @@
 package com.burninglove.dma.burninglove;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +13,10 @@ import android.widget.TextView;
 public class HasilActivity extends AppCompatActivity {
 
     private TextView tv_hasil;
+    private TextView tv_totalCal;
     private Button btn_back;
+
+    private double burned;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,7 @@ public class HasilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hasil);
 
         tv_hasil = (TextView) findViewById(R.id.tv_hasil);
+        tv_totalCal = (TextView) findViewById(R.id.tv_totalCal);
         btn_back = (Button) findViewById(R.id.btn_back);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
@@ -28,12 +35,13 @@ public class HasilActivity extends AppCompatActivity {
         });
 
         initHasil();
+        initSharedPreferences();
+        initTotal();
     }
 
-    protected void initHasil() {
+    private void initHasil() {
         String type = getIntent().getStringExtra("type");
         int counter = getIntent().getIntExtra("counter", 0);
-        double burned = 0;
 
         if (type.equals("pushup")) {
             burned = counter * 0.3;
@@ -42,6 +50,25 @@ public class HasilActivity extends AppCompatActivity {
         }
 
         tv_hasil.setText("" + burned + " Cal");
+    }
+
+    private void initSharedPreferences() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+
+        String cal = pref.getString("Calorie", "0");
+        double dCal = Double.parseDouble(cal);
+        cal = dCal + burned + "";
+
+        editor.putString("Calorie", "" + cal);
+        editor.apply();
+    }
+
+    private void initTotal() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String cal = pref.getString("Calorie", "0");
+        double dCal = Double.parseDouble(cal);
+        tv_totalCal.setText( String.format( "%.1f", dCal ) );
     }
 
     @Override
--- app/src/main/java/com/burninglove/dma/burninglove/Statistics.java
+++ app/src/main/java/com/burninglove/dma/burninglove/Statistics.java
@@ -1,13 +1,29 @@
 package com.burninglove.dma.burninglove;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Statistics extends AppCompatActivity {
+
+    private TextView tv_calorie_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        tv_calorie_value = (TextView) findViewById(R.id.tv_calorie_value);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+//        pref.edit().clear().commit();
+
+        String cal = pref.getString("Calorie", "0");
+        double dCal = Double.parseDouble(cal);
+        tv_calorie_value.setText( String.format( "%.1f", dCal ) );
     }
 }
